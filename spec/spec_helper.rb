0c0450205fe8a005df1b233728ca13f3ddcbf4e9
Andrii Balitskyi <10balian10@gmail.com>
2024-05-02 14:27:46 +0200

    Update rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,13 @@
 require "simplecov"
 require "simplecov-console"
 
+
 SimpleCov.start
 
 require "seam"
+require "webmock/rspec"
+
+require "support/helpers"
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
@@ -20,4 +24,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include Helpers
 end
