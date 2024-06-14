74c9cb543ad517e6c0a58533dd9f7eab366e5603
Andrii Balitskyi <10balian10@gmail.com>
2024-06-14 13:45:53 +0200

    Bump the generator to get client and resource imports, update top level seam imports

--- lib/seam.rb
+++ lib/seam.rb
@@ -8,15 +8,8 @@ require_relative "seam/client"
 require_relative "seam/base_client"
 require_relative "seam/base_resource"
 
-require_relative "seam/routes/resources/resource_errors_support"
-require_relative "seam/routes/resources/resource_warnings_support"
-
-Dir["#{__dir__}/seam/routes/resources/*.rb"].each do |file|
-  next if file.end_with?("resource_errors_support.rb", "resource_warnings_support.rb")
-
-  require_relative file
-end
-Dir["#{__dir__}/seam/routes/clients/*.rb"].each { |file| require_relative file }
+require_relative "seam/routes/resources/index"
+require_relative "seam/routes/clients/index"
 
 module Seam
 end
