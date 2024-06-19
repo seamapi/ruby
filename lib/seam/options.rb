cc20860073d6eba9f8e38a88f4f7d3585b3442e8
Andrii Balitskyi <10balian10@gmail.com>
2024-06-19 17:23:13 +0200

    Update options.rb following ruby's conventions

--- lib/seam/options.rb
+++ lib/seam/options.rb
@@ -11,18 +11,15 @@ module Seam
     seam_api_url = ENV["SEAM_API_URL"]
     seam_endpoint = ENV["SEAM_ENDPOINT"]
 
-    warn_deprecated_usage if seam_api_url
-    warn_conflicting_usage if seam_api_url && seam_endpoint
+    if seam_api_url
+      warn "\033[93mUsing the SEAM_API_URL environment variable is deprecated. Support will be removed in a later major version. Use SEAM_ENDPOINT instead.\033[0m"
+    end
+
+    if seam_api_url && seam_endpoint
+      warn "\033[93mDetected both the SEAM_API_URL and SEAM_ENDPOINT environment variables. Using SEAM_ENDPOINT.\033[0m"
+    end
 
     seam_endpoint || seam_api_url
-  end
-
-  def warn_deprecated_usage
-    puts "\033[93mUsing the SEAM_API_URL environment variable is deprecated. Support will be removed in a later major version. Use SEAM_ENDPOINT instead.\033[0m"
-  end
-
-  def warn_conflicting_usage
-    puts "\033[93mDetected both the SEAM_API_URL and SEAM_ENDPOINT environment variables. Using SEAM_ENDPOINT.\033[0m"
   end
 
   class SeamHttpInvalidOptionsError < StandardError
@@ -31,25 +28,33 @@ module Seam
     end
   end
 
-  def is_seam_http_options_with_api_key(api_key: nil, personal_access_token: nil)
-    if api_key && personal_access_token
-      raise SeamHttpInvalidOptionsError,
-        "The personal_access_token option cannot be used with the api_key option"
+  def seam_http_options_with_api_key?(api_key: nil, personal_access_token: nil)
+    return false if api_key.nil?
+
+    if personal_access_token
+      raise SeamHttpInvalidOptionsError.new(
+        'The personal_access_token option cannot be used with the api_key option'
+      )
     end
 
-    !api_key.nil?
+    true
   end
 
-  def is_seam_http_options_with_personal_access_token(personal_access_token: nil, api_key: nil, workspace_id: nil)
-    if api_key && personal_access_token
-      raise SeamHttpInvalidOptionsError,
-        "The api_key option cannot be used with the personal_access_token option"
-    end
-    if personal_access_token && workspace_id.nil?
-      raise SeamHttpInvalidOptionsError,
-        "Must pass a workspace_id when using a personal_access_token"
+  def seam_http_options_with_personal_access_token?(personal_access_token: nil, api_key: nil, workspace_id: nil)
+    return false if personal_access_token.nil?
+
+    if api_key
+      raise SeamHttpInvalidOptionsError.new(
+        'The api_key option cannot be used with the personal_access_token option'
+      )
     end
 
-    !personal_access_token.nil?
+    if workspace_id.nil?
+      raise SeamHttpInvalidOptionsError.new(
+        'Must pass a workspace_id when using a personal_access_token'
+      )
+    end
+
+    true
   end
 end
