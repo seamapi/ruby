302f86ef982037cfe13d0b80d5407c464c9346c6
Andrii Balitskyi <10balian10@gmail.com>
2024-06-19 17:16:17 +0200

    Update token.rb following ruby's conventions

--- lib/seam/token.rb
+++ lib/seam/token.rb
@@ -2,44 +2,48 @@
 
 module Seam
   TOKEN_PREFIX = "seam_"
+
   ACCESS_TOKEN_PREFIX = "seam_at"
+
   JWT_PREFIX = "ey"
+
   CLIENT_SESSION_TOKEN_PREFIX = "seam_cst"
+
   PUBLISHABLE_KEY_TOKEN_PREFIX = "seam_pk"
 
-  def is_access_token(token)
+  def access_token?(token)
     token.start_with?(ACCESS_TOKEN_PREFIX)
   end
 
-  def is_jwt(token)
+  def jwt?(token)
     token.start_with?(JWT_PREFIX)
   end
 
-  def is_seam_token(token)
+  def seam_token?(token)
     token.start_with?(TOKEN_PREFIX)
   end
 
-  def is_api_key(token)
-    !is_client_session_token(token) &&
-      !is_jwt(token) &&
-      !is_access_token(token) &&
-      !is_publishable_key(token) &&
-      is_seam_token(token)
+  def api_key?(token)
+    !client_session_token?(token) &&
+      !jwt?(token) &&
+      !access_token?(token) &&
+      !publishable_key?(token) &&
+      seam_token?(token)
   end
 
-  def is_client_session_token(token)
+  def client_session_token?(token)
     token.start_with?(CLIENT_SESSION_TOKEN_PREFIX)
   end
 
-  def is_publishable_key(token)
+  def publishable_key?(token)
     token.start_with?(PUBLISHABLE_KEY_TOKEN_PREFIX)
   end
 
-  def is_console_session_token(token)
-    is_jwt(token)
+  def console_session_token?(token)
+    jwt?(token)
   end
 
-  def is_personal_access_token(token)
-    is_access_token(token)
+  def personal_access_token?(token)
+    access_token?(token)
   end
 end
