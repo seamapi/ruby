24ef2d524babbf066e9f07845fb9353d44571142
Andrii Balitskyi <10balian10@gmail.com>
2024-06-24 16:28:26 +0200

    Test seam init

---
+++ spec/seam_client/init_seam_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe Seam::Client do
+  let(:seam) { Seam::Client.new(api_key: "seam_some_api_key") }
+
+  describe "#initialize" do
+    it "initializes Seam with fixture" do
+      expect(seam.lts_version).not_to be_nil
+      expect(seam.wait_for_action_attempt).to be_falsey
+    end
+  end
+end
