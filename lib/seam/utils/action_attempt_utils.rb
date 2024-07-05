3bea1d73eca095e7522572ae7e959a7c421c7f24
Andrii Balitskyi <10balian10@gmail.com>
2024-07-05 11:09:42 +0200

    Add util methods for action attempt polling

---
+++ lib/seam/utils/action_attempt_utils.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "seam/utils/action_attempt_utils"
+
+module Seam
+  module Utils
+    module ActionAttemptUtils
+      def self.decide_and_wait(action_attempt, client, wait_for_action_attempt)
+        wait_decision = wait_for_action_attempt.nil? ? client.defaults["wait_for_action_attempt"] : wait_for_action_attempt
+
+        if wait_decision == true
+          wait_until_finished(action_attempt, client)
+        elsif wait_decision.is_a?(Hash)
+          wait_until_finished(action_attempt, client, timeout: wait_decision[:timeout],
+            polling_interval: wait_decision[:polling_interval])
+        end
+      end
+
+      def self.wait_until_finished(action_attempt, client, timeout: 5.0, polling_interval: 0.5)
+        time_waiting = 0.0
+
+        while action_attempt.status == "pending"
+          sleep(polling_interval)
+          time_waiting += polling_interval
+
+          raise "Timed out waiting for action attempt to be finished" if time_waiting > timeout
+
+          action_attempt = update_action_attempt(action_attempt, client)
+
+          raise "Action Attempt failed: #{action_attempt.error["message"]}" if action_attempt.status == "failed"
+        end
+
+        action_attempt
+      end
+
+      def self.update_action_attempt(action_attempt, client)
+        response = client.request_seam(
+          :post,
+          "/action_attempts/get",
+          body: {
+            action_attempt_id: action_attempt.action_attempt_id
+          }
+        )
+
+        action_attempt.update_from_response(response["action_attempt"])
+        action_attempt
+      end
+    end
+  end
+end
