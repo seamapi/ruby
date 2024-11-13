75a570cfcddd779d06fb9d7f79b8286b7f9f6f13
Andrii Balitskyi <10balian10@gmail.com>
2024-11-13 15:54:01 +0100

    Add basic test using fake-seam-connect

---
+++ spec/integration/basic_usage_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe "Basic SDK Usage" do
+  it "can list devices" do
+    with_fake_seam_connect do |seam, endpoint|
+      devices = seam.devices.list
+      expect(devices).to be_a(Array)
+      expect(devices).not_to be_nil
+      expect(devices.length).to be > 0
+
+      device = devices.first
+      expect(device).to be_a(Seam::Resources::Device)
+      expect(device.device_id).to be_a(String)
+      expect(device.created_at).to be_a(Time)
+    end
+  end
+end
