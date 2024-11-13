611117118c97aa72e5dad8cf6c5fbfd90201a4fc
Andrii Balitskyi <10balian10@gmail.com>
2024-11-13 17:14:24 +0100

    Update spec/integration/basic_usage_spec.rb

--- spec/integration/basic_usage_spec.rb
+++ spec/integration/basic_usage_spec.rb
@@ -2,7 +2,8 @@
 
 RSpec.describe "Basic SDK Usage" do
   it "can list devices" do
-    with_fake_seam_connect do |seam, endpoint|
+    with_fake_seam_connect do |_, endpoint, seed|
+      seam = Seam.new(api_key: seed["seam_apikey1_token"], endpoint: endpoint)
       devices = seam.devices.list
       expect(devices).to be_a(Array)
       expect(devices).not_to be_nil
