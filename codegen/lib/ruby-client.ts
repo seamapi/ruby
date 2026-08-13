1b2871bdb4a0b310e59a1bbe564e19ead24913d0
Evan Sosenko <evan@getseam.com>
2026-08-12 23:53:47 -0700

    feat: Use preferredMethod

--- codegen/lib/ruby-client.ts
+++ codegen/lib/ruby-client.ts
@@ -14,6 +14,7 @@ export interface ClientMethodParameter {
 
 export interface ClientMethod {
   methodName: string
+  httpMethod: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
   description: string
   isDeprecated: boolean
   deprecationMessage: string
